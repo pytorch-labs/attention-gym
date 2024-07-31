3c4f6dc8b0793c0dd9496ee3d2faf24b000ecf1b
drisspg <drisspguessous@gmail.com>
2024-07-31 15:12:46 -0700

    add sliding window

--- attn_gym/masks/__init__.py
+++ attn_gym/masks/__init__.py
@@ -1,1 +1,2 @@
 from attn_gym.masks.causal import causal_mask
+from attn_gym.masks.sliding_window import generate_sliding_window
---
+++ attn_gym/masks/sliding_window.py
@@ -0,0 +1,52 @@
+"""Generates a sliding window attention mask"""
+
+import torch
+from torch.nn.attention.flex_attention import _mask_mod_signature, and_masks
+from attn_gym.masks import causal_mask
+
+
+def generate_sliding_window(window_size: int) -> _mask_mod_signature:
+    """Generates a sliding window attention mask with a given window size.
+    Args:
+        window_size: The size of the sliding window.
+
+    Note:
+        We assume that the window size is represents the lookback size and we mask out all future tokens
+        similar to causal masking.
+    """
+
+    def sliding_window(b, h, q_idx, kv_idx):
+        return q_idx - kv_idx <= window_size
+
+    sliding_window_mask = and_masks(sliding_window, causal_mask)
+    sliding_window_mask.__name__ = f"sliding_window_{window_size}"
+    return sliding_window_mask
+
+
+def main(device: str = "cpu"):
+    """Visualize the attention scores of sliding window mask mod.
+
+    Args:
+        device (str): Device to use for computation. Defaults
+    """
+    from attn_gym import visualize_attention_scores
+
+    B, H, SEQ_LEN, HEAD_DIM = 1, 1, 12, 8
+
+    def make_tensor():
+        return torch.ones(B, H, SEQ_LEN, HEAD_DIM, device=device)
+
+    query, key = make_tensor(), make_tensor()
+
+    sliding_window_mask = generate_sliding_window(3)
+    visualize_attention_scores(
+        query, key, mask_mod=sliding_window_mask, device=device, name="sliding_window_mask"
+    )
+
+
+if __name__ == "__main__":
+    try:
+        from jsonargparse import CLI
+    except ImportError:
+        raise ImportError("Be sure to run: pip install -e .'[viz]'")
+    CLI(main)

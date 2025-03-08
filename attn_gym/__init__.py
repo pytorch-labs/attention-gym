1867e82e2dc57b9038a500fca31b7f66bf3d3b89
drisspg <drisspguessous@gmail.com>
2025-03-08 13:32:44 -0800

    Change import structure

--- attn_gym/__init__.py
+++ attn_gym/__init__.py
@@ -1,1 +1,6 @@
 from attn_gym.utils import visualize_attention_scores
+import attn_gym.mods
+import attn_gym.masks
+import attn_gym.paged_attention
+
+__all__ = ["visualize_attention_scores", "mods", "masks", "paged_attention"]

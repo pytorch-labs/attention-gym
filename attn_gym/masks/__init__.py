b8bfd57a621a7a7e29018306baa90dc6cd1f1520
yibozhong <yibozhong657@gmail.com>
2025-05-28 18:52:47 +0800

    Add Sliding Tile Attention impl

--- attn_gym/masks/__init__.py
+++ attn_gym/masks/__init__.py
@@ -4,3 +4,4 @@ from attn_gym.masks.prefix_lm import generate_prefix_lm_mask
 from attn_gym.masks.document_mask import generate_doc_mask_mod
 from attn_gym.masks.dilated_sliding_window import generate_dilated_sliding_window
 from attn_gym.masks.natten import generate_natten, generate_tiled_natten, generate_morton_natten
+from attn_gym.masks.sta import generate_sta_mask_mod_2d, generate_sta_mask_mod_3d
